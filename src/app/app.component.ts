0ba9b262d4def1f5f1bd4d6ea732473c367fdddd
ashwinee-mahajan <56250948+ashwinee-mahajan@users.noreply.github.com>
2020-06-12 21:55:02 +0100

    video meeting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -35,7 +35,7 @@ export class AppComponent implements OnInit {
 
   /** @type {MediaStreamConstraints} */
   constraints = {
-    audio: true,
+    audio: {'echoCancellation': true},
     video: { facingMode: 'user' },
   };
 
