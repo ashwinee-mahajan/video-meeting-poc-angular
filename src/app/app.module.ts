d4004a1089a833f57419facc32f1bea5cfb736e9
ashwinee-mahajan <56250948+ashwinee-mahajan@users.noreply.github.com>
2020-06-28 09:28:44 +0100

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 import { AppComponent } from './app.component';
+import { CanvasComponent } from './canvas/canvas.component';
 
 const config: SocketIoConfig = { url: 'https://node-video-server.herokuapp.com/', options: {}  };
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CanvasComponent
   ],
   imports: [
     BrowserModule,
